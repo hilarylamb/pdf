73bcda1cc3eb90d12416139a61dabf0b979d95f1
Hilary Lamb <hilaryklamb@gmail.com>
2020-03-11 21:07:50 -0400

    set imports in app.js created storage destination specification

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('hey');
+const express = require('express'); //using express to create routes
+const app = express(); //initialize express
+const fs = require('fs'); //used to read and create files
+const multer = require('multer'); //allow us to upload files to server
+const { TesseractWorker } = require('tesseract.js'); //read our images
+const worker = new TesseractWorker(); //analyze images with worker
+
+//save all images uploaded in created storage specify file name and where we want
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, './uploads'); //argument path to folder, set destination
+  },
+  filename: (req, res, cb) => {
+    cb(null, req.file);
+  }
+});
+
+//
+const upload = multer({ storage: storage }).single('avatar');
+
+app.set('view engine', 'ejs');
